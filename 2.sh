dd34fdc5927879bf752ad5194d100e354f343710
ajkpasala <pasajay2909@gmail.com>
2016-03-21 17:16:52 -0400

    adding build script v1

---
+++ 2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+echo " this is a project"
+echo "i need version number here in order to proced with next step please enter build version number"
+read buildVersion
+echo $buildVersion
+echo "build success"
